310e824e301fc8f3309879b2454ac335561aa34d
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2024-10-11 00:56:28 -0300

    fix

---
+++ webhook_server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    if "frontend" in data["repository"]["name"]:
+        os.system("ansible-playbook -i ansible/hosts ansible/frontend_deploy.yml")
+    elif "backend" in data["repository"]["name"]:
+        os.system("ansible-playbook -i ansible/hosts ansible/backend_deploy.yml")
+    return "Deploy acionado!", 200
+
+if __name__ == "__main__":
+    app.run(port=5000)
